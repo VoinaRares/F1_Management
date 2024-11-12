41adea1e0b91de315f0cca7f9233fb0dbcb9b9ba
VoinaRares <voinarares14@gmail.com>
2024-11-12 16:00:37 +0200

    Added Getter and Setter for Engineer

--- src/main/com/consoleapp/model/Engineer.java
+++ src/main/com/consoleapp/model/Engineer.java
@@ -5,4 +5,20 @@ public class Engineer extends Person {
     private String specialty;
     private int  yearsWithCurrentTeam;
     // private Team team;
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public int getYearsWithCurrentTeam() {
+        return yearsWithCurrentTeam;
+    }
+
+    public void setYearsWithCurrentTeam(int yearsWithCurrentTeam) {
+        this.yearsWithCurrentTeam = yearsWithCurrentTeam;
+    }
 }
