b62c8e6832537390c0dddd23c2bd24c92104c15d
PaulS3rb <paulserb2@gmail.com>
2024-11-12 19:31:09 +0200

    added and shortly implemented Console.show_login_menu

--- src/main/com/consoleapp/presentation/Console.java
+++ src/main/com/consoleapp/presentation/Console.java
@@ -2,7 +2,13 @@ package main.com.consoleapp.presentation;
 
 public class Console {
 
-
+    public void show_login_menu()
+    {
+        System.out.println("\tF1 MANAGEMENT");
+        System.out.println("1.Login");
+        System.out.println("2.Sign up");
+        System.out.println("3.Exit");
+    }
 
     public void run()
     {
