f69470db01e2da55387bf689bdfc170a3aff2d02
VoinaRares <voinarares14@gmail.com>
2024-11-12 15:23:57 +0200

    Added the abstract Person class for inheritance

---
+++ src/main/com/consoleapp/model/Person.java
@@ -0,0 +1,60 @@
+package main.com.consoleapp.model;
+
+public abstract class Person {
+    private int id;
+    private String name;
+    private int age;
+    private int experience;
+    private float salary;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, int age, int experience, float salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
