0f38257b950d7a12e23e168ccf0a6fff52a7f717
VoinaRares <voinarares14@gmail.com>
2024-11-12 16:00:10 +0200

    Added Driver

---
+++ src/main/com/consoleapp/model/Driver.java
@@ -0,0 +1,14 @@
+package main.com.consoleapp.model;
+
+public class Driver extends Person {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    //private Team team;
+}
