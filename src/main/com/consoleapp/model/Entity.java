6e15b955abc313ef71fa2881a2f80d22384c0282
PaulS3rb <paulserb2@gmail.com>
2024-11-12 19:02:11 +0200

    added constructor with getter and setter

--- src/main/com/consoleapp/model/Entity.java
+++ src/main/com/consoleapp/model/Entity.java
@@ -1,4 +1,17 @@
 package main.com.consoleapp.model;
 
-public class Entity {
+public abstract class Entity {
+    int id;
+    Entity(int id)
+    {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
