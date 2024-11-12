40a4a2da9e4f00c5b5fa48009ff3fb2967ec9acb
VoinaRares <voinarares14@gmail.com>
2024-11-12 18:49:55 +0200

    Updated F1Admin to include methods and attributes

--- src/main/com/consoleapp/model/F1Admin.java
+++ src/main/com/consoleapp/model/F1Admin.java
@@ -2,5 +2,26 @@ package main.com.consoleapp.model;
 
 public class F1Admin extends Person {
 
-    //To add methods for the admin
+    private Calendar calendar;
+
+    public F1Admin(int id, String name, int age, int experience, float salary, Calendar calendar) {
+        super(id, name, age, experience, salary);
+        this.calendar = calendar;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public void organizeRaces(Race startPoint, Race endPoint) {
+
+    }
+
+    public void addRace(){
+
+    }
 }
