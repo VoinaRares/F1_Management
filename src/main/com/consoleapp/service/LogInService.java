37b44ca03a3acc8a6a6c2088891f671dc0937bbc
PaulS3rb <paulserb2@gmail.com>
2024-11-12 22:43:50 +0200

    added an example for each Person type

--- src/main/com/consoleapp/service/LogInService.java
+++ src/main/com/consoleapp/service/LogInService.java
@@ -9,7 +9,10 @@ public class LogInService {
     public LogInService()
     {
         TeamManager teamManager=new TeamManager(0,"Toto Wolff",50,10,2500, 0, "EX1","y");
-        Driver  driver= new Driver(1,"Charles Leclerc", 25, 6, 1200000, 16, )
+        Driver  driver= new Driver(1,"Charles Leclerc", 25, 6, 1200000, 16,2,"EX2","y" );
+        Engineer engineer=new Engineer(2, "Adrian Newey", 65, 22, 122555, "Aerodynamics",
+                5, 0, "EX3","y");
+        F1Admin adminho= new F1Admin(3,"Adminho",25, 3,2000,"EX4","y");
     }
     InMemoryRepository<Person>repository=new InMemoryRepository<Person>();
 
