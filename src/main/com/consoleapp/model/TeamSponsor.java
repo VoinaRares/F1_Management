1004e98696f9c06c9c9f90ac499375520c07f5d7
PaulS3rb <paulserb2@gmail.com>
2024-11-12 18:50:49 +0200

    Added TeamSponsor constructor

--- src/main/com/consoleapp/model/TeamSponsor.java
+++ src/main/com/consoleapp/model/TeamSponsor.java
@@ -1,4 +1,12 @@
 package main.com.consoleapp.model;
 
 public class TeamSponsor {
+    private Team team;
+    private Sponsor sponsor;
+
+    TeamSponsor(Team team, Sponsor sponsor) {
+        this.team = team;
+        this.sponsor = sponsor;
+    }
+
 }
