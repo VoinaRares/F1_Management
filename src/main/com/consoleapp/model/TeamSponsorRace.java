3f35922c910ca595e8026f6db86300aae824a8d6
PaulS3rb <paulserb2@gmail.com>
2024-11-19 22:55:22 +0200

    continued implementing TeamSponsorRace class

--- src/main/com/consoleapp/model/TeamSponsorRace.java
+++ src/main/com/consoleapp/model/TeamSponsorRace.java
@@ -1,4 +1,12 @@
 package main.com.consoleapp.model;
 
-public class TeamSponsorRace {
+public class TeamSponsorRace extends Entity {
+    int TeamSponsorId;
+    int RaceId;
+
+    public TeamSponsorRace(int id, int teamSponsorId, int raceId) {
+        super(id);
+        this.TeamSponsorId = teamSponsorId;
+        this.RaceId = raceId;
+    }
 }
