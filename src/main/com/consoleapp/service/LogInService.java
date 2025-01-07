514026e17c20caae61b1b6351f3eff3c649fe6bf
PaulS3rb <paulserb2@gmail.com>
2025-01-07 19:29:00 +0200

    added more examples for inMemoryRepository

--- src/main/com/consoleapp/service/LogInService.java
+++ src/main/com/consoleapp/service/LogInService.java
@@ -118,9 +118,22 @@ public class LogInService {
     }
 
     private void populateInMemory(){
-        TeamManager teamManager=new TeamManager(1,"Toto Wolff",50,10,2500, 1, "1","y");
-        F1Admin adminho= new F1Admin(3,"Adminho",25, 3,2000,"4","y");
-        Team team1 = new Team(1,"Mercedes", 150000);
+        F1Admin adminho= new F1Admin(3,"Adminho",25, 3,2000,"00","y");
+        TeamManager teamManager1 =new TeamManager(1,"Toto Wolff",50,10,2500, 1,
+                "1","y");
+        TeamManager teamManager2 = new TeamManager(2, "Fred Vasseur", 60, 20, 5000,
+                2, "2", "y");
+        TeamManager teamManager3 = new TeamManager(3, "Christian Horner", 60, 20,
+                8000, 3, "3","y");
+        TeamManager teamManager4 = new TeamManager(4, "Andrea Stella", 60, 20, 1500,
+                4, "4","y");
+        TeamManager teamManager5 = new TeamManager(5, "Laurent Mekies", 60, 20, 1500,
+                5, "5","y");
+        Team team1 = new Team(1,"Mercedes", 150000000);
+        Team team2= new Team(2, "Ferrari", 150000000);
+        Team team3= new Team(3, "Red Bull", 150000000);
+        Team team4 = new Team(4, "McLaren", 150000000);
+        Team team5 = new Team(5, "Toro Rosso", 150000000);
         Location location1= new Location(120,"Italy","Europe",500,800);
         Race race1=new Race(50,location1);
         Location location2= new Location(121,"France","Europe",400,900);
@@ -139,16 +152,51 @@ public class LogInService {
         Race race8=new Race(57, location8);
         Location location9 = new Location(128, "Canada", "North America", -1800, 1000);
         Race race9 = new Race(58, location9);
+        Location location10 = new Location(129, "Australia", "Australia", 2000, -1000);
+        Race race10 = new Race(59, location10);
+
+
+        Engineer engineer1 = new Engineer(1, "James Alisson", 50, 15, 2000, "Engine",
+                5, 1, "a", "y");
+
+        Engineer engineer2 = new Engineer(2, "Papa Giovanni", 50, 15, 2000, "Engine",
+                2, 2, "a", "y");
+
+        Engineer engineer3 = new Engineer(3, "Adrian Newey", 55, 20, 12000, "Aerodynamics",
+                2, 3, "a", "y");
+
+        Engineer engineer4 = new Engineer(4, "Valentino Chiesa", 50, 15, 2000, "Engine",
+                 7, 2, "a", "y");
 
         Sponsor sponsor1 = new Sponsor(1, "Peroni", "Italy");
+        Sponsor sponsor2 = new Sponsor(2, "Louis Vuitton", "France");
+        Sponsor sponsor3 = new Sponsor(3, "Pepsi", "USA");
+        Sponsor sponsor4 = new Sponsor(4, "Red Bull", "Austria");
+        Sponsor sponsor5 = new Sponsor(5, "NTT Data", "Japan");
+        Sponsor sponsor6 = new Sponsor(6, "Shell", "UK");
+        Sponsor sponsor7 = new Sponsor(7, "Santander", "Spain");
 
-        Engineer engineer = new Engineer(1, "Adrian Newey", 55, 20, 12000, "Aerodynamics",
-                2, 1, "a", "y");
 
-        TeamSponsor teamSponsor = new TeamSponsor(1, 1, 1, 2500);
+
+        TeamSponsor teamSponsor1 = new TeamSponsor(1, 1, 2, 2500);
+        TeamSponsor teamSponsor2 = new TeamSponsor(2, 2, 6, 7000);
+        TeamSponsor teamSponsor3 = new TeamSponsor(3, 2, 7, 10000);
+        TeamSponsor teamSponsor4 = new TeamSponsor(4, 5, 3, 3000);
+        TeamSponsor teamSponsor5 = new TeamSponsor(5, 2, 1, 21000);
+        TeamSponsor teamSponsor6 = new TeamSponsor(6, 3, 4, 25000);
+        TeamSponsor teamSponsor7 = new TeamSponsor(7, 5, 4, 4000);
+        TeamSponsor teamSponsor8 = new TeamSponsor(8, 4, 5, 11000);
         repositoryF1Admin.create(adminho);
-        repositoryTeamManager.create(teamManager);
+        repositoryTeamManager.create(teamManager1);
+        repositoryTeamManager.create(teamManager2);
+        repositoryTeamManager.create(teamManager3);
+        repositoryTeamManager.create(teamManager4);
+        repositoryTeamManager.create(teamManager5);
         repositoryTeam.create(team1);
+        repositoryTeam.create(team2);
+        repositoryTeam.create(team3);
+        repositoryTeam.create(team4);
+        repositoryTeam.create(team5);
         repositoryRace.create(race1);
         repositoryRace.create(race2);
         repositoryRace.create(race3);
@@ -158,9 +206,28 @@ public class LogInService {
         repositoryRace.create(race7);
         repositoryRace.create(race8);
         repositoryRace.create(race9);
+        repositoryRace.create(race10);
         repositorySponsor.create(sponsor1);
-        repositoryEngineer.create(engineer);
-        repositoryTeamSponsor.create(teamSponsor);
+        repositorySponsor.create(sponsor2);
+        repositorySponsor.create(sponsor3);
+        repositorySponsor.create(sponsor4);
+        repositorySponsor.create(sponsor5);
+        repositorySponsor.create(sponsor6);
+        repositorySponsor.create(sponsor7);
+        repositoryEngineer.create(engineer1);
+        repositoryEngineer.create(engineer2);
+        repositoryEngineer.create(engineer3);
+        repositoryEngineer.create(engineer4);
+        repositoryTeamSponsor.create(teamSponsor1);
+        repositoryTeamSponsor.create(teamSponsor2);
+        repositoryTeamSponsor.create(teamSponsor3);
+        repositoryTeamSponsor.create(teamSponsor4);
+        repositoryTeamSponsor.create(teamSponsor5);
+        repositoryTeamSponsor.create(teamSponsor6);
+        repositoryTeamSponsor.create(teamSponsor7);
+        repositoryTeamSponsor.create(teamSponsor8);
+
+        ;
     }
 
 }
